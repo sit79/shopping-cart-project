dd49c1952cc7db7fe6f4e2bd66ab7c4b3ddaf771
sit79 <stefan.tolksdorf@gmail.com>
2019-06-03 13:38:41 +0200

    find all products & find product by ID added

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const logger = require("morgan")
 const mongoose = require("mongoose")
 const chalk = require("chalk")
 
-const indexRouter = require("./routes/index")
-// const usersRouter = require("./routes/users")
+const apiRouter = require("./routes/api")
 
 const app = express()
 const mongoUrl = "mongodb://localhost:27017/shop"
@@ -22,8 +21,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use("/", indexRouter)
-// app.use("/users", usersRouter)
+app.use("/", apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+const Product = require("../models/product")
+
+/* GET home page. */
+router.get("/", function(req, res) {
+  res.json({ response: "Welcome to this API" })
+})
+
+router.get("/api/all", async (req, res) => {
+  try {
+    const allProducts = await Product.find({})
+    res.status(200).send(allProducts)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.get("/api/:id", async (req, res) => {
+  try {
+    const allProducts = await Product.find({ _id: req.params.id })
+    res.status(200).send(allProducts)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+module.exports = router
