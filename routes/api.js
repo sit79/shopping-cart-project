4e10e3269c7bdef584ed64472747d1337ae9fb85
sit79 <stefan.tolksdorf@gmail.com>
2019-06-03 13:45:27 +0200

    route names modified

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Product = require("../models/product")
 
 /* GET home page. */
-router.get("/", function(req, res) {
+router.get("/api", function(req, res) {
   res.json({ response: "Welcome to this API" })
 })
 
