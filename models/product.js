a3d08e578f7f3bbb279d73829f2b9aadb38e1042
sit79 <stefan.tolksdorf@gmail.com>
2019-06-03 12:09:24 +0200

    model & seeder script added, db populated

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const ProductSchema = new Schema({
+  name: String,
+  description: String,
+  imgUrl: String,
+  price: Number
+})
+
+const Product = mongoose.model("Product", ProductSchema)
+module.exports = Product
---
+++ seed/seedProducts.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const faker = require("faker")
+const Product = require("../models/product")
+const chalk = require("chalk")
+
+mongoose.connect("mongodb://localhost/shop", { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error"))
+db.once("open", () => {
+  // Connection has been established
+  console.log(chalk.green("DB started…"))
+})
+
+const products = []
+let count = process.argv[2] || 20
+
+while (count) {
+  products.push({
+    name: faker.commerce.productName(),
+    description: faker.lorem.paragraph(),
+    imgUrl: faker.image.imageUrl(),
+    price: faker.commerce.price()
+  })
+
+  count--
+}
+
+Product.insertMany(products, (err, data) => {
+  if (err) return console.error(err)
+  mongoose.connection.close()
+  console.log(chalk.green("DB connection closed."))
+})
