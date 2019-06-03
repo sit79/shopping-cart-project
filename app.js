a5eb255a293e40083d7028bfdaf60793a2cb72d1
sit79 <stefan.tolksdorf@gmail.com>
2019-06-03 13:29:22 +0200

    nodemon added

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
+const mongoose = require("mongoose")
+const chalk = require("chalk")
 
 const indexRouter = require("./routes/index")
-const usersRouter = require("./routes/users")
+// const usersRouter = require("./routes/users")
 
 const app = express()
+const mongoUrl = "mongodb://localhost:27017/shop"
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
@@ -20,7 +23,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
-app.use("/users", usersRouter)
+// app.use("/users", usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +40,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500)
   res.render("error")
 })
+
+// mongoose connection
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log(chalk.green("CONNECTED TO MONGO"))
+  })
+  .catch(error => {
+    console.log(chalk.red("COULD NOT CONNECT", error))
+  })
 
 module.exports = app
