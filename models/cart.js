07df7ef74f3c4ab1e2859319005b3c7ec1fe2410
sit79 <stefan.tolksdorf@gmail.com>
2019-06-04 11:50:11 +0200

    adding and removing products

--- models/cart.js
+++ models/cart.js
@@ -1,17 +1,4 @@
-class ShoppingCart {
-  constructor() {
-    this.items = {}
-  }
-
-  add(product) {
-    const { _id: id } = product
-    if (this.items.hasOwnProperty(id)) {
-      this.items.id.quantity++
-    } else {
-      this.items[id] = { productDetails: { product }, quantity: 1 }
-    }
-  }
-}
+const chalk = require("chalk")
 
 const items = {
   "5cf5021a2df6635d380b9410": {
@@ -37,5 +24,68 @@ const items = {
       __v: 0
     },
     quantity: 1
+  },
+  "5cf5021a2df6635d380b9412": {
+    productDetails: {
+      _id: "5cf5021a2df6635d380b9412",
+      name: "Gorgeous Granite Shoes",
+      description:
+        "Cumque cum libero quas est ex accusantium reiciendis maiores omnis. Voluptatibus error quaerat aliquam reprehenderit accusantium corrupti sed. At nisi delectus quae quia quis eum.",
+      imgUrl: "http://lorempixel.com/640/480",
+      price: 389,
+      __v: 0
+    }
   }
 }
+
+class ShoppingCart {
+  constructor() {
+    this.items = {}
+  }
+
+  add(product) {
+    const { _id: id } = product.productDetails
+    if (this.items.hasOwnProperty(id)) {
+      console.log(chalk.blue(`${id} added`))
+      this.items[id].quantity++
+    } else {
+      console.log(chalk.green(`${id} created `))
+      this.items[id] = { ...product, quantity: 1 }
+    }
+  }
+
+  remove(product) {
+    const { _id: id } = product.productDetails
+    if (this.items.hasOwnProperty(id)) {
+      if (this.items[id].quantity > 1) {
+        this.items[id].quantity--
+        console.log(chalk.red(`${id} removed`))
+      } else {
+        delete this.items[id]
+        console.log(chalk.red(`${id} deleted`))
+      }
+    } else {
+      console.log(chalk.blue(`${id} is not in cart`))
+    }
+  }
+}
+
+const cart = new ShoppingCart()
+console.clear()
+console.log(cart)
+console.log("\n*********** OPERATIONS ******************\n")
+cart.add(items["5cf5021a2df6635d380b9411"])
+cart.add(items["5cf5021a2df6635d380b9411"])
+cart.add(items["5cf5021a2df6635d380b9410"])
+cart.add(items["5cf5021a2df6635d380b9410"])
+cart.add(items["5cf5021a2df6635d380b9410"])
+cart.add(items["5cf5021a2df6635d380b9410"])
+cart.add(items["5cf5021a2df6635d380b9410"])
+cart.remove(items["5cf5021a2df6635d380b9410"])
+cart.remove(items["5cf5021a2df6635d380b9410"])
+cart.remove(items["5cf5021a2df6635d380b9410"])
+cart.remove(items["5cf5021a2df6635d380b9410"])
+cart.remove(items["5cf5021a2df6635d380b9410"])
+console.log("\n*********** RESULTING CART **************\n")
+console.log(cart)
+console.log("\n*********** END *************************\n")
