5c8be48c4abead0d2b89e7009ee8420f0a326ea3
sit79 <stefan.tolksdorf@gmail.com>
2019-06-04 12:09:12 +0200

    cleaned up class methods to accept correct input object format

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,102 @@
 const chalk = require("chalk")
 
-const items = {
+const items = [
+  {
+    _id: "5cf5021a2df6635d380b9412",
+    name: "Gorgeous Granite Shoes",
+    description:
+      "Cumque cum libero quas est ex accusantium reiciendis maiores omnis. Voluptatibus error quaerat aliquam reprehenderit accusantium corrupti sed. At nisi delectus quae quia quis eum.",
+    imgUrl: "http://lorempixel.com/640/480",
+    price: 389,
+    __v: 0
+  },
+  {
+    _id: "5cf5021a2df6635d380b9415",
+    name: "Unbranded Plastic Bike",
+    description:
+      "Nisi modi fugiat voluptas. Labore corporis ullam et distinctio occaecati exercitationem. Inventore quam possimus omnis voluptatem eum voluptas. Ut omnis qui. Ullam voluptatibus velit.",
+    imgUrl: "http://lorempixel.com/640/480",
+    price: 759,
+    __v: 0
+  },
+  {
+    _id: "5cf5021a2df6635d380b9418",
+    name: "Refined Wooden Cheese",
+    description:
+      "Vitae nihil accusamus ad autem. Minus temporibus laudantium sunt corrupti est consequatur. Voluptatibus qui corrupti rerum id quibusdam ratione.",
+    imgUrl: "http://lorempixel.com/640/480",
+    price: 663,
+    __v: 0
+  },
+  {
+    _id: "5cf5021a2df6635d380b9419",
+    name: "Tasty Steel Soap",
+    description:
+      "Autem est qui id et qui ipsam nihil deserunt. Error est qui mollitia voluptatem vel porro. Magnam excepturi corporis cum magnam labore sapiente praesentium porro.",
+    imgUrl: "http://lorempixel.com/640/480",
+    price: 350,
+    __v: 0
+  }
+]
+
+class ShoppingCart {
+  constructor() {
+    this.items = {}
+  }
+
+  add(product) {
+    const { _id: id } = product
+    const item = this.items[id]
+    if (this.items.hasOwnProperty(id)) {
+      console.log(chalk.blue(`${id} added`))
+      item.quantity++
+      item.price = item.quantity * item.productDetails.price
+    } else {
+      console.log(chalk.green(`${id} created `))
+      this.items[id] = {
+        productDetails: product,
+        quantity: 1,
+        price: product.price
+      }
+    }
+  }
+
+  remove(product) {
+    const { _id: id } = product
+    const item = this.items[id]
+    if (this.items.hasOwnProperty(id)) {
+      if (item.quantity > 1) {
+        item.quantity--
+        item.price = item.quantity * item.productDetails.price
+        console.log(chalk.red(`${id} removed`))
+      } else {
+        delete this.items[id]
+        console.log(chalk.red(`${id} deleted`))
+      }
+    } else {
+      console.log(chalk.blue(`${id} is not in cart`))
+    }
+  }
+}
+
+const cart = new ShoppingCart()
+console.clear()
+console.log(cart)
+console.log("\n*********** OPERATIONS ******************\n")
+cart.add(items[0])
+cart.add(items[1])
+cart.add(items[1])
+cart.add(items[2])
+cart.add(items[2])
+cart.add(items[2])
+cart.remove(items[2])
+cart.remove(items[2])
+cart.remove(items[2])
+console.log("\n*********** RESULTING CART **************\n")
+console.log(cart)
+console.log("\n*********** END *************************\n")
+
+/* const SHOPPING-CART-EXAMPLE = {
   "5cf5021a2df6635d380b9410": {
     productDetails: {
       _id: "5cf5021a2df6635d380b9410",
@@ -36,56 +132,4 @@ const items = {
       __v: 0
     }
   }
-}
-
-class ShoppingCart {
-  constructor() {
-    this.items = {}
-  }
-
-  add(product) {
-    const { _id: id } = product.productDetails
-    if (this.items.hasOwnProperty(id)) {
-      console.log(chalk.blue(`${id} added`))
-      this.items[id].quantity++
-    } else {
-      console.log(chalk.green(`${id} created `))
-      this.items[id] = { ...product, quantity: 1 }
-    }
-  }
-
-  remove(product) {
-    const { _id: id } = product.productDetails
-    if (this.items.hasOwnProperty(id)) {
-      if (this.items[id].quantity > 1) {
-        this.items[id].quantity--
-        console.log(chalk.red(`${id} removed`))
-      } else {
-        delete this.items[id]
-        console.log(chalk.red(`${id} deleted`))
-      }
-    } else {
-      console.log(chalk.blue(`${id} is not in cart`))
-    }
-  }
-}
-
-const cart = new ShoppingCart()
-console.clear()
-console.log(cart)
-console.log("\n*********** OPERATIONS ******************\n")
-cart.add(items["5cf5021a2df6635d380b9411"])
-cart.add(items["5cf5021a2df6635d380b9411"])
-cart.add(items["5cf5021a2df6635d380b9410"])
-cart.add(items["5cf5021a2df6635d380b9410"])
-cart.add(items["5cf5021a2df6635d380b9410"])
-cart.add(items["5cf5021a2df6635d380b9410"])
-cart.add(items["5cf5021a2df6635d380b9410"])
-cart.remove(items["5cf5021a2df6635d380b9410"])
-cart.remove(items["5cf5021a2df6635d380b9410"])
-cart.remove(items["5cf5021a2df6635d380b9410"])
-cart.remove(items["5cf5021a2df6635d380b9410"])
-cart.remove(items["5cf5021a2df6635d380b9410"])
-console.log("\n*********** RESULTING CART **************\n")
-console.log(cart)
-console.log("\n*********** END *************************\n")
+}*/
