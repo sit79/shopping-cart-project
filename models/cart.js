0f9f396c19c4c105da85a86012e682155eb17fff
sit79 <stefan.tolksdorf@gmail.com>
2019-06-04 14:24:42 +0200

    getter for total quantity implemented

--- models/cart.js
+++ models/cart.js
@@ -87,6 +87,27 @@ class ShoppingCart {
       console.log(chalk.blue(`${id} not in cart`))
     }
   }
+
+  _isEmpty(obj) {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) return false
+    }
+    return true
+  }
+
+  get totalQty() {
+    const items = this.items
+    if (!this._isEmpty(items)) {
+      // count and return total quantity
+      let sum = 0
+      for (let key in items) {
+        sum += items[key].quantity
+      }
+      return sum
+    } else {
+      return 0
+    }
+  }
 }
 
 const cart = new ShoppingCart()
@@ -94,54 +115,69 @@ const cart = new ShoppingCart()
 // Playground
 
 console.clear()
+
+console.log("\n*********** CART AT START ***************\n")
+
 console.log(cart)
+
 console.log("\n*********** OPERATIONS ******************\n")
+
 cart.add(items[0])
 cart.add(items[0])
 cart.add(items[0])
+cart.add(items[1])
+cart.add(items[2])
+cart.add(items[3])
 cart.remove(items[0])
 cart.remove(items[0])
 cart.remove(items[0])
 cart.removeAll(items[1])
-// cart.removeAll(items[2])
+cart.removeAll(items[2])
+
 console.log("\n*********** RESULTING CART **************\n")
+
 console.log(cart)
+
+console.log("\n*********** TEST ************************\n")
+
+console.log(cart.totalQty)
+
 console.log("\n*********** END *************************\n")
 
-/* const SHOPPING-CART-EXAMPLE = {
-  "5cf5021a2df6635d380b9410": {
-    productDetails: {
-      _id: "5cf5021a2df6635d380b9410",
-      name: "Handmade Fresh Chicken",
-      description:
-        "A qui qui odit doloremque et quasi totam et. Assumenda ab id est odio. Omnis laborum voluptatem.",
-      imgUrl: "http://lorempixel.com/640/480",
-      price: 808,
-      __v: 0
-    },
-    quantity: 1
-  },
-  "5cf5021a2df6635d380b9411": {
-    productDetails: {
-      _id: "5cf5021a2df6635d380b9411",
-      name: "Small Granite Mouse",
-      description:
-        "Non ullam numquam eveniet quae et nihil nobis soluta. Porro debitis et nihil. Iure sed itaque et est cupiditate.",
-      imgUrl: "http://lorempixel.com/640/480",
-      price: 236,
-      __v: 0
-    },
-    quantity: 1
-  },
-  "5cf5021a2df6635d380b9412": {
-    productDetails: {
-      _id: "5cf5021a2df6635d380b9412",
-      name: "Gorgeous Granite Shoes",
-      description:
-        "Cumque cum libero quas est ex accusantium reiciendis maiores omnis. Voluptatibus error quaerat aliquam reprehenderit accusantium corrupti sed. At nisi delectus quae quia quis eum.",
-      imgUrl: "http://lorempixel.com/640/480",
-      price: 389,
-      __v: 0
-    }
-  }
-}*/
+// const SHOPPING-CART-EXAMPLE = {
+//   "5cf5021a2df6635d380b9410": {
+//     productDetails: {
+//       _id: "5cf5021a2df6635d380b9410",
+//       name: "Handmade Fresh Chicken",
+//       description:
+//         "A qui qui odit doloremque et quasi totam et. Assumenda ab id est odio. Omnis laborum voluptatem.",
+//       imgUrl: "http://lorempixel.com/640/480",
+//       price: 808,
+//       __v: 0
+//     },
+//     quantity: 1
+//   },
+//   "5cf5021a2df6635d380b9411": {
+//     productDetails: {
+//       _id: "5cf5021a2df6635d380b9411",
+//       name: "Small Granite Mouse",
+//       description:
+//         "Non ullam numquam eveniet quae et nihil nobis soluta. Porro debitis et nihil. Iure sed itaque et est cupiditate.",
+//       imgUrl: "http://lorempixel.com/640/480",
+//       price: 236,
+//       __v: 0
+//     },
+//     quantity: 1
+//   },
+//   "5cf5021a2df6635d380b9412": {
+//     productDetails: {
+//       _id: "5cf5021a2df6635d380b9412",
+//       name: "Gorgeous Granite Shoes",
+//       description:
+//         "Cumque cum libero quas est ex accusantium reiciendis maiores omnis. Voluptatibus error quaerat aliquam reprehenderit accusantium corrupti sed. At nisi delectus quae quia quis eum.",
+//       imgUrl: "http://lorempixel.com/640/480",
+//       price: 389,
+//       __v: 0
+//     }
+//   }
+// }
