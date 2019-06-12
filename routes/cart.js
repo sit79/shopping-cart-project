0c258bf7e132631c637769ad4f4fca1b6e5efcdf
sit79 <stefan.tolksdorf@gmail.com>
2019-06-12 13:15:58 +0200

    routes to all cart functions added

--- routes/cart.js
+++ routes/cart.js
@@ -5,18 +5,49 @@ const Product = require("../models/product")
 // Route Configuration
 const cartRoutes = {
   cart: "/",
-  addProductById: "/:id",
+  addProductById: "/add/:id",
+  removeProductById: "/remove/:id",
+  removeAllProductsById: "/remove-all/:id",
 }
 
-// Show all endpoints at home route (/)
+// show cart
+cartRouter.get(cartRoutes.cart, (req, res) => {
+  const { cart } = req.session
+  cart["totalQuantity"] = cart.totalQty
+  cart["totalPrice"] = cart.totalPrc
+  res.json(cart)
+})
+
+// addProductById
 cartRouter.get(cartRoutes.addProductById, async (req, res) => {
   const { id } = req.params
   const { cart } = req.session
-
   const product = await Product.findById(id)
-
   cart.add(product)
+  cart["totalQuantity"] = cart.totalQty
+  cart["totalPrice"] = cart.totalPrc
+  res.json({ cart })
+})
 
+// removeProductById
+cartRouter.get(cartRoutes.removeProductById, async (req, res) => {
+  const { id } = req.params
+  const { cart } = req.session
+  const product = await Product.findById(id)
+  cart.remove(product)
+  cart["totalQuantity"] = cart.totalQty
+  cart["totalPrice"] = cart.totalPrc
+  res.json({ cart })
+})
+
+// removeAllProductsById
+cartRouter.get(cartRoutes.removeAllProductsById, async (req, res) => {
+  const { id } = req.params
+  const { cart } = req.session
+  const product = await Product.findById(id)
+  cart.removeAll(product)
+  cart["totalQuantity"] = cart.totalQty
+  cart["totalPrice"] = cart.totalPrc
   res.json({ cart })
 })
 
